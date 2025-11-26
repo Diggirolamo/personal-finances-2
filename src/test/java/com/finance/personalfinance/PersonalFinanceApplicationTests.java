2839cbd29d7f35e180149e3d1b977cfcdf1319fb
AndreaDiGirolamo <andrea.digirolam@gmail.com>
2025-11-26 17:58:22 +0100

    first commit si volaaa

---
+++ src/test/java/com/finance/personalfinance/PersonalFinanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finance.personalfinance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalFinanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
